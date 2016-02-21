98764ae90176a7fc8adf7f298258176bc046c4d0
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2016-02-20 23:25:58 -0300

    Copy of BestFitDistribution, Measurement and DurationMeasurement from project AdaptmeWebMaven (removal of jaxb errors)

--- src/main/java/model/spem/measurement/DurationMeasurement.java
+++ src/main/java/model/spem/measurement/DurationMeasurement.java
@@ -2,65 +2,49 @@ package model.spem.measurement;
 
 import java.io.Serializable;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 import model.spem.Sample;
 import model.spem.util.TimeEnum;
 
 @Entity
 @DiscriminatorValue("duration measurement") // opcional
-@XmlRootElement(name = "duration measurement")
+@XmlRootElement(name="duration_measurement")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class DurationMeasurement extends Measurement implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 
-    // @Id
-    // @GeneratedValue(strategy=GenerationType.IDENTITY)
-    // private Integer id;
+	private double duration;
+	
+ 
+	@Enumerated(EnumType.STRING)
+	private TimeEnum scale;
 
-    private double duration;
+ 
+	public double getValue() {
+		return duration;
+	}
 
-    @ManyToOne
-    private Sample sample;
+	public void setValue(double value) {
+		this.duration = value;
+	}
 
-    @Enumerated(EnumType.STRING)
-    private TimeEnum scale;
+ 
+	public TimeEnum getScale() {
+		return scale;
+	}
 
-    // public Integer getId() {
-    // return id;
-    // }
-    //
-    // public void setId(Integer id) {
-    // this.id = id;
-    // }
-
-    public double getValue() {
-	return duration;
-    }
-
-    public void setValue(double value) {
-	this.duration = value;
-    }
-
-    public Sample getSample() {
-	return sample;
-    }
-
-    public void setSample(Sample sample) {
-	this.sample = sample;
-    }
-
-    public TimeEnum getScale() {
-	return scale;
-    }
-
-    public void setScale(TimeEnum scale) {
-	this.scale = scale;
-    }
+	public void setScale(TimeEnum scale) {
+		this.scale = scale;
+	}
+	
+	@Override
+		public String toString() {
+			return Double.toString(duration) + " " + scale;
+		}
 }
--- src/main/java/model/spem/measurement/Measurement.java
+++ src/main/java/model/spem/measurement/Measurement.java
@@ -10,45 +10,72 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlIDREF;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlTransient;
 
 import model.spem.Sample;
 import model.spem.util.MeasurementType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "MEASUREMENT_TYPE")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="MEASUREMENT_TYPE")
+@XmlRootElement(name="measurement")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlSeeAlso({DurationMeasurement.class, RoleMeasurement.class, WorkProductMeasurement.class})
+//@XmlSeeAlso(DurationMeasurement.class)
+
 public class Measurement implements Serializable {
+	
+	 
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	
+	@ManyToOne
+	@XmlTransient 
+	private Sample sample;
 
-    @ManyToOne
-    private Sample sample;
-
-    public Sample getSample() {
-	return sample;
-    }
-
-    public void setSample(Sample sample) {
-	this.sample = sample;
-    }
-
-    public static Measurement createMeasurement(MeasurementType measurementType) {
-	Measurement measurement = null;
-	switch (measurementType) {
-	case DURATION_MEASUREMENT:
-	    measurement = new DurationMeasurement();
-	    break;
-	case ROLE_MEASUREMENT:
-	    measurement = new RoleMeasurement();
-	    break;
-	case WORK_PRODUCT_MEASUREMENT:
-	    measurement = new WorkProductMeasurement();
-	    break;
+	public Sample getSample() {
+		return sample;
 	}
 
-	return measurement;
-    }
+	public void setSample(Sample sample) {
+		this.sample = sample;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static Measurement createMeasurement(MeasurementType measurementType) {
+		Measurement measurement = null;
+		switch (measurementType) {
+		case DURATION_MEASUREMENT:
+			measurement = new DurationMeasurement();
+			break;
+		case ROLE_MEASUREMENT:
+			measurement = new RoleMeasurement();
+			break;
+		case WORK_PRODUCT_MEASUREMENT:
+			measurement = new WorkProductMeasurement();
+			break;
+		}
+	 
+		return measurement;	 
+	}
 
 }
