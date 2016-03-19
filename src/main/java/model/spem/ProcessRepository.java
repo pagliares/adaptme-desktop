8210922a8c1ea13eac2f8758f88e55ec6d85fc8f
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2016-03-19 11:02:04 -0300

    Replacement of ProcessRepository in AdaptMe with ProcessRepository from AdaptMeWebMaven (changes made to accomodate list of tasks only on JSF

--- src/main/java/model/spem/ProcessRepository.java
+++ src/main/java/model/spem/ProcessRepository.java
@@ -12,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -27,77 +28,80 @@ import model.spem.util.ProcessContentType;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ProcessRepository implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @XmlAttribute
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@XmlAttribute
+	private Integer id;
 
-    @XmlAttribute
-    private String name = "Teste process name";
-    
-    @XmlAttribute
-    private String simulationObjective;
+	@XmlAttribute
+	private String name = "Teste process name";
 
+	@XmlAttribute
+	private String simulationObjective;
 
-    @XmlAttribute
-    private boolean chosen;
+	@XmlAttribute
+	private boolean chosen;
 
-    @XmlElementRef(name = "process_content")
-    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "processRepository")
-    private List<ProcessContentRepository> processContents;
+	@XmlElementRef(name = "process_content")
+	@OneToMany(cascade = CascadeType.PERSIST, mappedBy = "processRepository")
+	private List<ProcessContentRepository> processContents;
 
-    @ManyToOne
-    @XmlTransient
-    private SimulationFacade simulationFacade;
-    
-    @XmlTransient
-    // faltar ignorar JPA
-    private List<ProcessContentRepository> listTasks = new ArrayList<>();
+	@ManyToOne
+	@XmlTransient
+	private SimulationFacade simulationFacade;
 
-    public ProcessRepository() {
-	processContents = new ArrayList<>();
-    }
+	@XmlTransient
+	@Transient
+	private List<ProcessContentRepository> listProcessContentRepositoryWithTasksOnly = new ArrayList<>();
 
-    public String getName() {
-	return name;
-    }
+	@XmlTransient
+	@Transient
+	private List<ProcessContentRepository> tasks = new ArrayList<>();
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public ProcessRepository() {
+		processContents = new ArrayList<>();
+	}
 
-    public boolean isChosen() {
-	return chosen;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setChosen(boolean chosen) {
-	this.chosen = chosen;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public SimulationFacade getSimulationFacade() {
-	return simulationFacade;
-    }
+	public boolean isChosen() {
+		return chosen;
+	}
 
-    public void setSimulationFacade(SimulationFacade simulationFacade) {
-	this.simulationFacade = simulationFacade;
-    }
+	public void setChosen(boolean chosen) {
+		this.chosen = chosen;
+	}
 
-    public boolean addProcessElement(ProcessContentRepository processContentsRepository) {
-	return processContents.add(processContentsRepository);
-    }
+	public SimulationFacade getSimulationFacade() {
+		return simulationFacade;
+	}
 
-    public List<ProcessContentRepository> getProcessContents() {
-	return processContents;
-    }
+	public void setSimulationFacade(SimulationFacade simulationFacade) {
+		this.simulationFacade = simulationFacade;
+	}
 
-    public void setProcessContents(List<ProcessContentRepository> processContents) {
-	this.processContents = processContents;
-    }
+	public boolean addProcessElement(ProcessContentRepository processContentsRepository) {
+		return processContents.add(processContentsRepository);
+	}
 
-    @Override
-    public String toString() {
-	return name;
-    }
+	public List<ProcessContentRepository> getProcessContents() {
+		return processContents;
+	}
+
+	public void setProcessContents(List<ProcessContentRepository> processContents) {
+		this.processContents = processContents;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
 
 	public String getSimulationObjective() {
 		return simulationObjective;
@@ -106,43 +110,85 @@ public class ProcessRepository implements Serializable {
 	public void setSimulationObjective(String simulationObjective) {
 		this.simulationObjective = simulationObjective;
 	}
-	
-	// With this return, i can get list of roles, additional roles, input/output workproducts
-	public List<ProcessContentRepository> getListProcessContentRepositoryWithTasksOnly(List<ProcessContentRepository> listOfProcessContentRepository) {
- 		if (listOfProcessContentRepository == null) {
+
+	// With this return, i can get list of roles, additional roles, input/output
+	// workproducts
+	public List<ProcessContentRepository> getListProcessContentRepositoryWithTasksOnly(
+			List<ProcessContentRepository> listOfProcessContentRepository) {
+		if (listOfProcessContentRepository == null) {
 			return null;
 		} else {
-			for (ProcessContentRepository pcr: listOfProcessContentRepository ) {
+			for (ProcessContentRepository pcr : listOfProcessContentRepository) {
 				if (pcr.getType().equals(ProcessContentType.TASK)) {
-					listTasks.add(pcr);
+					listProcessContentRepositoryWithTasksOnly.add(pcr);
 				} else
-					getListProcessContentRepositoryWithTasksOnly(pcr.getChildren());		 
+					getListProcessContentRepositoryWithTasksOnly(pcr.getChildren());
 			}
-			return listTasks;
+			return listProcessContentRepositoryWithTasksOnly;
 		}
 	}
-	
-	// Este metodo removeu um erro muito dificil que era a geracao de varias tarefas no xacdml duplicada
-	public void clearListOfTasks() {
-		listTasks.clear();
+
+	public List<ProcessContentRepository> getTasks(List<ProcessContentRepository> listOfProcessContentRepository) {
+		if (listOfProcessContentRepository == null) {
+			return null;
+		} else {
+			for (ProcessContentRepository pcr : listOfProcessContentRepository) {
+				if (pcr.getType().equals(ProcessContentType.TASK)) {
+					listProcessContentRepositoryWithTasksOnly.add(pcr);
+				} else
+					getListProcessContentRepositoryWithTasksOnly(pcr.getChildren());
+			}
+			return listProcessContentRepositoryWithTasksOnly;
+		}
 	}
-	
+
+	// Este metodo removeu um erro muito dificil que era a geracao de varias
+	// tarefas no xacdml duplicada
+	public void clearListOfTasks() {
+		listProcessContentRepositoryWithTasksOnly.clear();
+	}
+
+	public List<ProcessContentRepository> getListProcessContentRepositoryWithParticularProcessContentOnly(
+			List<ProcessContentRepository> listOfProcessContentRepository, String processContentName) {
+		if (listOfProcessContentRepository == null) {
+			return null;
+		} else {
+			for (ProcessContentRepository pcr : listOfProcessContentRepository) {
+				if (pcr.getName().equalsIgnoreCase(processContentName)) {
+					listProcessContentRepositoryWithTasksOnly.add(pcr);
+				} else
+					getListProcessContentRepositoryWithTasksOnly(pcr.getChildren());
+			}
+			return listProcessContentRepositoryWithTasksOnly;
+		}
+	}
+
 	public void imprimeTasks(List<ProcessContentRepository> listOfProcessContentRepository) {
-		 
+
 		if (listOfProcessContentRepository == null) {
 			return;
 		} else {
-			for (ProcessContentRepository pcr: listOfProcessContentRepository ) {
+			for (ProcessContentRepository pcr : listOfProcessContentRepository) {
 				if (pcr.getType().equals(ProcessContentType.TASK)) {
 					System.out.println(pcr.getName());
 				} else
-					imprimeTasks(pcr.getChildren()); 
+					imprimeTasks(pcr.getChildren());
 			}
-		 
+
 		}
 	}
-	
-	
-	
+
+	public void setListProcessContentRepositoryWithTasksOnly(
+			List<ProcessContentRepository> listProcessContentRepositoryWithTasksOnly) {
+		this.listProcessContentRepositoryWithTasksOnly = listProcessContentRepositoryWithTasksOnly;
+	}
+
+	public List<ProcessContentRepository> getTasks() {
+		return getTasks(processContents);
+	}
+
+	public void setTasks(List<ProcessContentRepository> tasks) {
+		this.tasks = tasks;
+	}
 	
 }
