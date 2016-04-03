53bebb9990730d560a51684000b0440261b09720
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2016-04-03 11:33:58 -0300

    creation of TestExperimentationProgramProxy to easy tests

---
+++ src/test/java/adaptme/TestExperimentationProgramProxy.java
@@ -0,0 +1,11 @@
+package adaptme;
+
+public class TestExperimentationProgramProxy {
+
+	public static void main(String[] args) {
+		ExperimentationProgramProxy experimentationProgramProxy = new ExperimentationProgramProxy();
+		experimentationProgramProxy.execute();
+
+	}
+
+}
