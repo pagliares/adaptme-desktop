75e80c03c9193d96d0703cebd46b745cf0007b73
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2016-09-08 20:30:34 -0300

    [Wladmir code] minor refactoring onsn class IntPriorityQ, a kind of InServiceEntities controller

--- src/main/java/simula/IntPriorityQ.java
+++ src/main/java/simula/IntPriorityQ.java
@@ -1,88 +1,85 @@
 // Arquivo IntPriorityQ.java
-// Implementação das Classes do Grupo de Modelagem da Biblioteca de Simulação JAVA
+// Implementaï¿½ï¿½o das Classes do Grupo de Modelagem da Biblioteca de Simulaï¿½ï¿½o JAVA
 // 9.Abr.1999	Wladimir
 
 package simula;
 
 import java.util.*;
 
-class IntPriorityQ
-{
-	private Vector q;					// implementa fila como vetor
+class IntPriorityQ {
+	private Vector queue;					// implementa fila como vetor
 
 	/**
-	 * constrói uma fila vazia com capacidade ilimitada. 
+	 * constrï¿½i uma fila vazia com capacidade ilimitada. 
 	 */
-	public IntPriorityQ()
-	{
-		q = new Vector(5, 5);	
+	public IntPriorityQ(){
+		queue = new Vector(5, 5);	
 	}
 	
 	/**
-	 * coloca no fim da fila de acordo com o instante de serviço
+	 * coloca no fim da fila de acordo com o instante de serviï¿½o
 	 */
-	public void Enqueue(IntQEntry e)
-	{
-		int min, max, cur;	// max pode ser negativo (qdo for inserir no começo)
-		IntQEntry e2;
-		// encontra posição de inserção baseado no tempo de serviço de IntQEntry e.
-		// implementa busca binária, já que os elementos esão ordenados por tempo.
+	public void Enqueue(InServiceEntities inServiceEntities){
+		int min, max, cur;	// max pode ser negativo (qdo for inserir no comeï¿½o)
+		InServiceEntities inServiceEntities2;
+		// encontra posiï¿½ï¿½o de inserï¿½ï¿½o baseado no tempo de serviï¿½o de IntQEntry e.
+		// implementa busca binï¿½ria, jï¿½ que os elementos esï¿½o ordenados por tempo.
 		min = 0;
-		max = q.size() - 1;
+		max = queue.size() - 1;
 		cur = 0;
-		while(min <= max)
-		{
+		while(min <= max){
 			cur = (min + max) / 2;
-			e2 = (IntQEntry)q.elementAt(cur);
-			if(e.duetime < e2.duetime)
+			inServiceEntities2 = (InServiceEntities)queue.elementAt(cur);
+			if(inServiceEntities.duetime < inServiceEntities2.duetime)
 				max = cur - 1;
-			else if(e.duetime >= e2.duetime)				// após os de mesma prioridade 
-				min = ++cur;								// que já estão na fila
+			else if(inServiceEntities.duetime >= inServiceEntities2.duetime)				// apï¿½s os de mesma prioridade 
+				min = ++cur;								// que jï¿½ estï¿½o na fila
 		}
-		// cur contém a posição de inserção
-		q.insertElementAt(e, cur);
+		// cur contï¿½m a posiï¿½ï¿½o de inserï¿½ï¿½o
+		queue.insertElementAt(inServiceEntities, cur);
 	}
 	
 	/**
-	 * recoloca na cabeça da fila;
-	 * não é checado o tempo de serviço, portanto só deve ser usada se for para elemento que
-	 * acabou de ser retirado através de Dequeue
+	 * recoloca na cabeï¿½a da fila;
+	 * nï¿½o ï¿½ checado o tempo de serviï¿½o, portanto sï¿½ deve ser usada se for para elemento que
+	 * acabou de ser retirado atravï¿½s de Dequeue
 	 */
-	public void PutBack(IntQEntry e)
-	{	
-		q.insertElementAt(e, 0);
+	public void PutBack(InServiceEntities e){	
+		queue.insertElementAt(e, 0);
 	}
 	
 	/**
-	 * retira da cabeça da fila.
+	 * retira da cabeï¿½a da fila.
 	 */
-	public IntQEntry Dequeue()
-	{
-		try
-		{
-			IntQEntry e = (IntQEntry)q.firstElement();
-			q.removeElementAt(0);
-			return e;
+	public InServiceEntities Dequeue(){
+		try{
+			InServiceEntities inServiceEntities = (InServiceEntities)queue.firstElement();
+			queue.removeElementAt(0);
+			return inServiceEntities;
 		}
-		catch(NoSuchElementException x){return null;}
+		catch(NoSuchElementException x){
+			return null;
+		}
 	}
 
 	/**
 	 * retira do fim da fila
 	 */
-	public IntQEntry FromTail()
-	{
-		try
-		{
-			IntQEntry e = (IntQEntry)q.lastElement();
-			q.removeElementAt(q.size() - 1);
+	public InServiceEntities FromTail(){
+		try{
+			InServiceEntities e = (InServiceEntities)queue.lastElement();
+			queue.removeElementAt(queue.size() - 1);
 			return e;
 		}
-		catch(NoSuchElementException x){return null;}
+		catch(NoSuchElementException x){
+			return null;
+		}
 	}
 	
 	/**
-	 * retorna true se fila está vazia
+	 * retorna true se fila estï¿½ vazia
 	 */
-	public boolean IsEmpty(){return q.isEmpty();}
+	public boolean IsEmpty(){
+		return queue.isEmpty();
+	}
 }
