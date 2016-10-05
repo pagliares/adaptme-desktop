568f0c2e2f807b5dbaf1c54e93a5ad420ec4ea05
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2016-10-05 20:28:09 -0300

    Elimination of warm-up period widgets on experimentation panel and inclusion of verification of empty or less than 1 values for duration and replication textfields. Backup with warm-um widgets on class ExperimentationPanelWithRunInPeriod.java

--- src/main/java/adaptme/ui/window/perspective/ExperimentationPanelWithRunInPeriod.java
+++ src/main/java/adaptme/ui/window/perspective/ExperimentationPanelWithRunInPeriod.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JScrollPane;
@@ -39,63 +38,89 @@ import javax.swing.DefaultCellEditor;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.FlowLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
-public class ExperimentationPanel extends JPanel {
+public class ExperimentationPanelWithRunInPeriod extends JPanel {
 	
 	
 	private JTextField simulationDurationTextField;
 	private JTable table;
 	private JTextField numberOfReplicationsTextField;
+	private JTextField textField_2;
 	private TableColumnModel modeloColuna;
 	private JComboBox<VariableType> comboBoxVariableType = new JComboBox<>();
+	private JTextField textField_3;
 	
 	private ShowResultsPanel showResultsPanel;
 	private SimulationManagerFacade simulationManagerFacade;
 	
 	private WorkProductResourcesPanel workProductResourcesPanel;
+	private JTextField textField_4;
 	
 	private JTabbedPane tabbedPaneActivity4;
 
-	public ExperimentationPanel(WorkProductResourcesPanel workProductResourcesPanel, JTabbedPane tabbedPaneActivity4, SimulationFacade simulationFacade) {
+	public ExperimentationPanelWithRunInPeriod(WorkProductResourcesPanel workProductResourcesPanel, JTabbedPane tabbedPaneActivity4, SimulationFacade simulationFacade) {
 		this.tabbedPaneActivity4 = tabbedPaneActivity4;
 		this.workProductResourcesPanel = workProductResourcesPanel;
 		this.simulationManagerFacade = SimulationManagerFacade.getSimulationManagerFacade(); // singleton
 		this.simulationManagerFacade.setShowResultsPanel(showResultsPanel);
+		setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Run-in period. Welch's method (non-terminating systems)", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBounds(584, 22, 495, 69);
+		add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblWindowFrame = new JLabel("Time window");
+		lblWindowFrame.setBounds(20, 33, 89, 16);
+		panel_1.add(lblWindowFrame);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(122, 27, 75, 28);
+		panel_1.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblRuninTime = new JLabel("Run-in time");
+		lblRuninTime.setBounds(300, 33, 89, 16);
+		panel_1.add(lblRuninTime);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(389, 27, 69, 28);
+		panel_1.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Line chart for run-in period (Welch's method)", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_3.setBounds(584, 191, 495, 295);
+		 
+		add(panel_3);
+		panel_3.setLayout(null);
 		
 		JPanel welchChartPanel = new WelchChartPanel().createChartPanel();
 		welchChartPanel.setBounds(6, 23, 483, 266);
+		panel_3.add(welchChartPanel);
 		 
 		 
 		
 		JPanel panel_4 = new JPanel();
 		panel_4.setBorder(new TitledBorder(null, "Run control", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_4.setBounds(32, 417, 522, 69);
+		add(panel_4);
+		panel_4.setLayout(null);
 		
 		JLabel lblNumberOfReplications = new JLabel("Number of replications");
+		lblNumberOfReplications.setBounds(6, 29, 145, 16);
+		panel_4.add(lblNumberOfReplications);
 		
 		numberOfReplicationsTextField = new JTextField();
+		numberOfReplicationsTextField.setBounds(163, 23, 66, 28);
+		panel_4.add(numberOfReplicationsTextField);
 		numberOfReplicationsTextField.setColumns(10);
 		
 		JButton btnSimulate = new JButton("Simulate");
 		btnSimulate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				if(numberOfReplicationsTextField.getText().trim().isEmpty() || (Integer.parseInt(numberOfReplicationsTextField.getText()) < 1)){
-					JOptionPane.showMessageDialog(getPanel(), "The number of replications is required");
-					return; 
-				}
-				
-				if((simulationDurationTextField.getText().trim().isEmpty()) || (Integer.parseInt(simulationDurationTextField.getText()) < 1)){
-					JOptionPane.showMessageDialog(getPanel(), "The duration of the simulation is is required when simulation is not finished by lack of temporary entities.");
-					return; 
-				}
-				
 				int numberReplications = Integer.parseInt(numberOfReplicationsTextField.getText());
-				int durationTime = Integer.parseInt(simulationDurationTextField.getText());
-				
 				showResultsPanel.updateShowResultsPanelTable(numberReplications);
 				simulationFacade.addNumberOfSimulationRuns(numberReplications);
 				simulationManagerFacade.execute(Float.parseFloat(simulationDurationTextField.getText()), numberReplications); // TODO buscar do text field
@@ -104,80 +129,53 @@ public class ExperimentationPanel extends JPanel {
 				
 			}
 		});
-		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
-		gl_panel_4.setHorizontalGroup(
-			gl_panel_4.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_4.createSequentialGroup()
-					.addGap(155)
-					.addComponent(lblNumberOfReplications)
-					.addGap(5)
-					.addComponent(numberOfReplicationsTextField, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-					.addGap(194)
-					.addComponent(btnSimulate)
-					.addGap(398))
-		);
-		gl_panel_4.setVerticalGroup(
-			gl_panel_4.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_4.createSequentialGroup()
-					.addGap(11)
-					.addComponent(lblNumberOfReplications))
-				.addGroup(gl_panel_4.createSequentialGroup()
-					.addGap(5)
-					.addGroup(gl_panel_4.createParallelGroup(Alignment.BASELINE)
-						.addComponent(numberOfReplicationsTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnSimulate)))
-		);
-		panel_4.setLayout(gl_panel_4);
+		btnSimulate.setBounds(256, 24, 98, 29);
+		panel_4.add(btnSimulate);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "System type", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_6.setBounds(32, 344, 522, 61);
+		add(panel_6);
+		panel_6.setLayout(null);
+		
+		JCheckBox chckbxStopOnB = new JCheckBox("Non-terminating");
+		chckbxStopOnB.setBounds(18, 23, 165, 23);
+		panel_6.add(chckbxStopOnB);
+		
+		JCheckBox chckbxStopOnEnd = new JCheckBox("Terminating");
+		chckbxStopOnEnd.setBounds(195, 23, 189, 23);
+		panel_6.add(chckbxStopOnEnd);
 		
 		JPanel panel_7 = new JPanel();
 		panel_7.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Project ending condition", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_7.setBounds(32, 272, 522, 69);
+		add(panel_7);
+		panel_7.setLayout(null);
 		
 		JLabel lblDurationTime = new JLabel("Duration time");
+		lblDurationTime.setBounds(21, 33, 87, 16);
+		panel_7.add(lblDurationTime);
 		
 		simulationDurationTextField = new JTextField();
+		simulationDurationTextField.setBounds(118, 27, 74, 28);
+		panel_7.add(simulationDurationTextField);
 		simulationDurationTextField.setColumns(10);
 		
-		JLabel lblWorkProductFinishes = new JLabel("No more temporary entity");
+		JLabel lblWorkProductFinishes = new JLabel("No more work product");
+		lblWorkProductFinishes.setBounds(204, 32, 165, 16);
+		panel_7.add(lblWorkProductFinishes);
 		
 		JComboBox comboBox = new JComboBox();
-		GroupLayout gl_panel_7 = new GroupLayout(panel_7);
-		gl_panel_7.setHorizontalGroup(
-			gl_panel_7.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_7.createSequentialGroup()
-					.addGap(155)
-					.addComponent(lblDurationTime)
-					.addGap(12)
-					.addComponent(simulationDurationTextField, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
-					.addGap(253)
-					.addComponent(lblWorkProductFinishes, GroupLayout.PREFERRED_SIZE, 165, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 230, GroupLayout.PREFERRED_SIZE)
-					.addGap(77))
-		);
-		gl_panel_7.setVerticalGroup(
-			gl_panel_7.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_7.createSequentialGroup()
-					.addGroup(gl_panel_7.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_7.createSequentialGroup()
-							.addGap(19)
-							.addComponent(lblDurationTime))
-						.addGroup(gl_panel_7.createSequentialGroup()
-							.addGap(13)
-							.addComponent(simulationDurationTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_panel_7.createSequentialGroup()
-							.addGap(19)
-							.addGroup(gl_panel_7.createParallelGroup(Alignment.BASELINE)
-								.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblWorkProductFinishes))))
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		panel_7.setLayout(gl_panel_7);
+		comboBox.setBounds(362, 28, 156, 27);
+		panel_7.add(comboBox);
 		
 		comboBoxVariableType.addItem(VariableType.INDEPENDENT);
 		comboBoxVariableType.addItem(VariableType.DEPENDENT);
 		comboBoxVariableType.addItem(VariableType.INTERMEDIATE);
 		
 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(32, 22, 522, 238);
+		add(scrollPane);
 		
 		table = new JTable();
 		
@@ -191,29 +189,25 @@ public class ExperimentationPanel extends JPanel {
 		configuraColunas();
  
 		scrollPane.setViewportView(table);
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(32)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 1037, Short.MAX_VALUE)
-						.addComponent(panel_7, GroupLayout.DEFAULT_SIZE, 1077, Short.MAX_VALUE)
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 1077, Short.MAX_VALUE))
-					.addContainerGap())
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(22)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 238, GroupLayout.PREFERRED_SIZE)
-					.addGap(12)
-					.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
-					.addGap(12)
-					.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)
-					.addGap(15))
-		);
-		setLayout(groupLayout);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null), "Pilot run control", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBounds(584, 103, 496, 69);
+		add(panel);
+		
+		JLabel label = new JLabel("Number of replications");
+		label.setBounds(6, 29, 145, 16);
+		panel.add(label);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(163, 23, 66, 28);
+		panel.add(textField_4);
+		
+		JButton btnSimulatePilot = new JButton("Simulate pilot");
+		btnSimulatePilot.setBounds(256, 24, 137, 29);
+		panel.add(btnSimulatePilot);
 
 	}
 	
@@ -246,9 +240,6 @@ public class ExperimentationPanel extends JPanel {
 		return simulationManagerFacade;
 	}
 
-	public JPanel getPanel() {
-		return this;
-	}
 	
 
 	
