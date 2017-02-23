de3f72ebbbbcdf2c8071e1c3987c9b51b0fbc19f
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2017-02-23 10:05:35 -0300

    some refactorings on SimulationManagerFacade in order to prepare for outputing simulation results in an aggregated manner. Only working for one replication

--- src/main/java/adaptme/facade/SimulationManagerFacade.java
+++ src/main/java/adaptme/facade/SimulationManagerFacade.java
@@ -102,6 +102,7 @@ public class SimulationManagerFacade {
 
 	}
 
+	// TODO remove this method after finishing the code to extend XACDML in order to support iteration and releases
 	public void printObserversReportByIteration(HashMap firstHash) {
 		Collection<String> chaves = firstHash.keySet();
 
@@ -261,6 +262,54 @@ public class SimulationManagerFacade {
 		String temp = printResultadosGlobalTextArea(matrizResultados, resultadoGlobalString, mapQueueVariableType );
 		return temp;
 	}
+	
+	/**
+	 * Algorithm 1 - Determines the dimension of the bidimensional array of
+	 * results 
+	 * 2- Creates the two-dimension array 
+	 * 3 - For each experiment in
+	 * resultadoGlobalSimulacao 
+	 * 		3.1 - extract the quantity of entities in each queue and stores it in the array 
+	 * 4 - Print array contents
+	 */
+	public String getResultadosGlobalStringFromConsole() {
+		
+		String resultadoGlobalString = "";
+
+		// quatro linhas abaixo apenas para descobrir o numero de linhas e
+		// colunas da matriz
+		Set<String> keys = resultadoGlobal.keySet(); // contem o nome de todos
+														// experimentos como
+														// chave
+		int numeroExperimentos = keys.size();
+		HashMap hm = (HashMap) resultadoGlobal.get(keys.iterator().next());
+		Set<String> chaves = hm.keySet();
+		int numeroFilas = chaves.size();
+		int[][] matrizResultados = new int[numeroExperimentos][numeroFilas];
+		int soma = 0;
+		int quantity = 0;
+		int contadorLinhas = 0; // number of entities in each queue
+		int contadorColunas = 0; // experimentos
+
+		for (String experimento : keys) {
+ 
+			HashMap secondHash = (HashMap) resultadoGlobal.get(experimento);
+			Collection<String> chavesNomeFilasSegundoHash = secondHash.keySet();
+
+			for (String queueName : chavesNomeFilasSegundoHash) {
+				if (secondHash.get(queueName) instanceof QueueEntry) {
+					QueueEntry qe1 = (QueueEntry) secondHash.get(queueName);
+					quantity = qe1.deadState.getCount();
+					matrizResultados[contadorLinhas][contadorColunas] = quantity; // nao armazeno o nome do experimento. Apenas inteiros com #entities																 
+				}
+				contadorColunas++;
+			}
+			contadorLinhas++;
+			contadorColunas = 0;
+		}
+		String temp = printResultadosGlobalTextAreaFromConsole(matrizResultados, resultadoGlobalString);
+		return temp;
+	}
 
 	private String printResultadosGlobalTextArea(int[][] matrizResultados, String resultadoGlobalString, Map<String, VariableType> mapQueueVariableType) {
 //		String cabecalho = "";
@@ -306,7 +355,74 @@ public class SimulationManagerFacade {
 		return cabecalho + resultadoGlobalString;
 	}
 	
+	private String printResultadosGlobalTextAreaFromConsole(int[][] matrizResultados, String resultadoGlobalString) {
+//		String cabecalho = "";
+//		if (mapQueueVariableType.size() !=0) {
+		    String cabecalho = "\n............................   PRINTING THE MEAN AND STANDARD DEVIATION OF ENTITIES IN EACH QUEUE CONFIGURED AS 'DEPENDENT' FOR EACH REPLICATION   ............................ \n";
+//		} else {
+//			cabecalho = "";
+//		}
+			
+		Set<String> keys = resultadoGlobal.keySet(); // contem o nome de todos experimentos como chave
+
+		HashMap secondHash = (HashMap) resultadoGlobal.get(keys.iterator().next());
+		Collection<String> chavesNomeFilasSegundoHash = secondHash.keySet();
+
+		double soma = 0.0;
+		StandardDeviation sd = new StandardDeviation();
+
+		Iterator iterator = chavesNomeFilasSegundoHash.iterator();
+		double[] numeroEntidadesPorFila = new double[matrizResultados.length];
+
+		for (int j = 0; j < matrizResultados[0].length; j++) { // para uma determinada coluna (celula representa quantidade de entities)
+																
+			String fila = (String) iterator.next();
+//			System.out.println(mapQueueVariableType.toString());
+//			if (mapQueueVariableType.containsKey(fila)) {
+				resultadoGlobalString += "\nQueue: " + fila;
+			 
+				for (int i = 0; i < matrizResultados.length; i++) { // somo todas as linhas para a coluna acima (representa experimentos)				 
+					resultadoGlobalString += "\n\tquantity in replication " + (i + 1) + "..: " + matrizResultados[i][j];
+					numeroEntidadesPorFila[i] = matrizResultados[i][j];
+					soma = soma + matrizResultados[i][j];
+				}
+				
+				double mean = Math.round(((soma / matrizResultados.length)*100.0)/100.0);
+				double standardDeviation = Math.round(((sd.evaluate(numeroEntidadesPorFila))*100.0)/100.0);
+				
+				resultadoGlobalString += "\n\tmean of entities in queue " + fila + "..:" + mean;
+				resultadoGlobalString += "\tStandard deviation..: " + standardDeviation + "\n";
+				soma = 0.0;
+				numeroEntidadesPorFila = new double[matrizResultados.length];
+			}
+//		}
+		return cabecalho + resultadoGlobalString;
+	}
+	
 	public String getResultadosCabecalho() {
+		
+		String resultadoGlobalString = "\n............   NUMBER OF SIMULATION RUNS, MEAN(SD) OF PROJECT DURATION, MEAN(SD) OF ITERATION/RELEASE (ONLY FOR ITERATIVE AND RELEASE BASED PROCESSES)   ............\n";
+ 		
+		resultadoGlobalString+= "\nNumber of simulation runs......................:  "  + getNumberOfSimulationRuns() + "\n";
+		
+		resultadoGlobalString+= "Number of days.....................................:  " + Math.round(simulationManagerFacade.getAverageNumberOfDays()*100.0)/100.0 +
+                "(" + Math.round(simulationManagerFacade.calculateStandardDeviationNumberOfDays()*100.0)/100.0 + ")" + "\n";
+ 
+		if (getSimulationManager().getScheduler().hasRelease()) {
+			resultadoGlobalString+= "Number of releases................................:  " + Math.round(simulationManagerFacade.getAverageNumberOfReleases()*100.0)/100.0 +
+					"(" + Math.round(simulationManagerFacade.calculateStandardDeviationNumberOfReleases()*100.0)/100.0 + ")" + "\n";
+		}
+		
+		if (getSimulationManager().getScheduler().hasIteration()) {
+			resultadoGlobalString+= "Number of iterations per release.............:  " + Math.round(simulationManagerFacade.getAverageNumberOfIterations()/simulationManagerFacade.getAverageNumberOfReleases()*100.0)/100.0 + 
+					"(" + Math.round(simulationManagerFacade.calculateStandardDeviationNumberOfReleases()*100.0)/100.0 + ")" + "\n";; // TODO implementar para release  
+		}
+		
+		return resultadoGlobalString;
+	}
+	
+	
+	public String getResultadosCabecalhoFromConsole() {
 		
 		String resultadoGlobalString = "\n............   NUMBER OF SIMULATION RUNS, MEAN(SD) OF PROJECT DURATION, MEAN(SD) OF ITERATION/RELEASE (ONLY FOR ITERATIVE AND RELEASE BASED PROCESSES)   ............\n";
  		
@@ -407,15 +523,16 @@ public class SimulationManagerFacade {
 		
 		double numberOfDays = 0.0;
 		double acumulatedNumberOfDays = 0.0;
-		double acumulatedNumberOfUserStories = 0.0;
-		double acumulateNumberOfIterations = 0.0;
-		double acumulateNumberOfReleases = 0.0;
+//		double acumulatedNumberOfUserStories = 0.0;
+//		double acumulateNumberOfIterations = 0.0;
+//		double acumulateNumberOfReleases = 0.0;
 		
 		this.numberOfSimulationRuns = numberReplications;
 		this.numberOfDaysPerReplication = new double[numberReplications];
-		this.numberOfReleasesPerReplication = new double[numberReplications];
-		this.numberOfproducedUserStoriesPerReplication = new double[numberReplications];
-		this.numberOfIterationsPerReplication = new double[numberReplications];
+		
+//		this.numberOfReleasesPerReplication = new double[numberReplications];
+//		this.numberOfproducedUserStoriesPerReplication = new double[numberReplications];
+//		this.numberOfIterationsPerReplication = new double[numberReplications];
 
 		System.out.println("\n\t\t\tDETAILED RESULTS BY REPLICATION - IMPLEMENTED BY PAGLIARES\n");
 
@@ -423,39 +540,33 @@ public class SimulationManagerFacade {
 			
 			dynamicExperimentationProgramProxy = DynamicExperimentationProgramProxyFactory.newInstance();
 			dynamicExperimentationProgramProxy.setSimulationDuration(simulationDuration);
-			dynamicExperimentationProgramProxy.execute(simulationDuration);
+			
+			dynamicExperimentationProgramProxy.execute(simulationDuration); // Execute the replication
 			
 			this.simulationManager = (SimulationManager) dynamicExperimentationProgramProxy.getSimulationManager(); // nao funciona no construtor
-																						
-			// simulationManager.outputSimulationResultsConsoleAsImplementedByWladimir();  
-			
-			// output to console implemented by Pagliares
-			// Imprime numero de dias, armazena a replicacao corrente em um mapa para posterior calculo agregado
 				
+			// 	simulationManager.outputSimulationResultsConsoleAsImplementedByWladimir();  
+ 			
+			// output to console implemented by Pagliares
+			// Print the number of days, stores the current replication in a map for further aggregate computation
 			
 			System.out.println("\nReplication (execution) #" + (i + 1) + "\n");
 			
-			// Computo do numero de dias da simulacao desta replicacao e armazenamento para posterior calculo da media
-			numberOfDays = simulationManager.getScheduler().GetClock() / 480;
+			// Calculates the the number of days of the simulation in this replication and stores for further average computation
+			numberOfDays = simulationManager.getScheduler().GetClock() / 480; 
 			numberOfDaysPerReplication[i] = numberOfDays;											
 			acumulatedNumberOfDays += numberOfDays;
 			System.out.println("\tProject duration: " + numberOfDays + " days\n"); // 480 minutes = 1 day 
 			
-			// saida de todos dead states
             printNumberOfEntitiesInEachDeadState();
 
-			// armazena o estado de todos os dead states desta replicacao para o computo da media por dead state no final de todas as replicacoes
+			// Stores the state of all dead states in this replication in order to compute the mean for dead states at the end of all replications
 			resultadoGlobal.put("run #" + (i + 1), queues);
 
-//			acumulateNumberOfIterations = reportByIteration(acumulateNumberOfIterations, i);
-//
-//			acumulateNumberOfReleases = reportByRelease(acumulateNumberOfReleases, i);
+			//acumulateNumberOfIterations = reportByIteration(acumulateNumberOfIterations, i);
+			//acumulateNumberOfReleases = reportByRelease(acumulateNumberOfReleases, i);
+			//printGlobalResults(numberReplications, acumulatedNumberOfDays, acumulatedNumberOfUserStories,acumulateNumberOfIterations, acumulateNumberOfReleases);
 
-//			printGlobalResults(numberReplications, acumulatedNumberOfDays, acumulatedNumberOfUserStories,acumulateNumberOfIterations, acumulateNumberOfReleases);
-
-			
-
-			
 			if ( !isFromConsole) {
 				
 				int currentProessAlternativeIndex = simulationFacade.getProcessAlternatives().size() - 1;
@@ -465,7 +576,11 @@ public class SimulationManagerFacade {
                  //	String selectedProcessAlternativeName = showResultsPanel.getSelectedProcessAlternativeName();
 				resultsSimulationMapAdaptMe.put(selectedProcessAlternativeName + i, getResultadosCabecalho()+ "\n" + 
                         getResultadosGlobalString(experimentationPanel.getMapQueueVariableType()));
-			}	 
+			} else {
+ 				resultsSimulationMap.put("Replication" + i, dynamicExperimentationProgramProxy); // armazena replicacoes
+				resultsSimulationMapAdaptMe.put("Replication" + i, getResultadosCabecalhoFromConsole()+ "\n" + 
+						getResultadosGlobalStringFromConsole());  
+			}
 
 			houseCleaning();
 		}
@@ -475,6 +590,7 @@ public class SimulationManagerFacade {
 //		 printResultadosGlobal();
 	}
 
+	// TODO remove this method after finishing the code to extend XACDML in order to support iteration and releases
 	private void printGlobalResults(int numberReplications, double acumulatedNumberOfDays,
 			double acumulatedNumberOfUserStories, double acumulateNumberOfIterations,
 			double acumulateNumberOfReleases) {
@@ -488,6 +604,7 @@ public class SimulationManagerFacade {
 		System.out.println("\tAverage number of days ..: " + averageNumberOfDays);
 	}
 
+	// TODO remove this method after finishing the code to extend XACDML in order to support iteration and releases
 	private double reportByRelease(double acumulateNumberOfReleases, int i) {
 		if (simulationManager.getScheduler().hasRelease()) {
 			System.out.println("number of releases ..: " + simulationManager.getScheduler().getNumberOfReleases());
@@ -501,6 +618,7 @@ public class SimulationManagerFacade {
 		return acumulateNumberOfReleases;
 	}
 
+	// TODO remove this method after finishing the code to extend XACDML in order to support iteration and releases
 	private double reportByIteration(double acumulateNumberOfIterations, int i) {
 		// Iteration and Release seen as ACD activities that does not advance the simulation clock
 		if (simulationManager.getScheduler().hasIteration()) {
@@ -527,7 +645,7 @@ public class SimulationManagerFacade {
 			System.out.println("\n\t\tQueue name : " + queueName);
 			QueueEntry qe = (QueueEntry) queues.get(queueName);
 
-			// ambas saidas abaixo retornam a variavel count
+			// Both below outputs return the count variable
 			System.out.println("\t\tNunber of entities in queue via getCount: " + qe.deadState.getCount());
 			// System.out.println("numero de entidadas na fila: via ObsLength, basta mudar simobj para o novo nome" + qe.SimObj.getCount());
 		}
