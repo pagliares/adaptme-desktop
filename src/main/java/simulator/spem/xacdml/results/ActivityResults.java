65148506686f914045243cbf7b3862c07f649ad4
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2017-04-10 16:34:01 -0300

    Removal of attribute that indicates the number of activities executed. It was not working and I decided to derive the number from the size of the list containing ActivityResults objects

--- src/main/java/simulator/spem/xacdml/results/ActivityResults.java
+++ src/main/java/simulator/spem/xacdml/results/ActivityResults.java
@@ -1,38 +1,23 @@
 package simulator.spem.xacdml.results;
 
 public class ActivityResults extends SPEMResults {
-
-	private int quantityOfActivities = 0;
 	
-	public ActivityResults(String activityName) {
+	private double timeActivityStarted;
+	private double timeActivityFinished;
+	
+	public ActivityResults(String activityName, double timeActivityStarted, double timeActivityFinished) {
 		super(activityName);
-		quantityOfActivities++;
-	}
-	
-	public int getQuantityOfActivities() {
-		return quantityOfActivities;
-	}
-	public void setQuantityOfActivities(int quantityOfActivities) {
-		this.quantityOfActivities = quantityOfActivities;
-	}
-	
-	public void addQuantityOfActivities() {
-		quantityOfActivities++;
-	}
-	
-	@Override
-	public String toString() {
-		String result = getName().split("_")[1] + " was executed  " + quantityOfActivities  +  " times";
-		return result;
+		this.timeActivityStarted = timeActivityStarted;
+		this.timeActivityFinished = timeActivityFinished;
 	}
 
 	@Override
 	public double getTimeWorkBreakdownElementStarted() {
-		return 0;
+		return timeActivityStarted;
 	}
 
 	@Override
 	public double getTimeWorkBreakdownElementFinished() {
-		return 0;
+		return timeActivityFinished;
 	}
 }
